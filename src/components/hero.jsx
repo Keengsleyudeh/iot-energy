3220bc1d8a9219654612088d23a56675cbc6f8a0
callzmary6 <callzmary6@gmail.com>
2024-12-29 18:59:50 +0100

    chore: change writeup in hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,47 +1,3 @@
-// import { chakra, Link, Stack, Box, Button, useColorModeValue } from '@chakra-ui/react';
-// // Here we have used react-icons package for the icon
-// import { FaGithub } from 'react-icons/fa';
-// import { BsDiscord } from 'react-icons/bs';
-
-// const Hero = ({displayName}) => {
-//   return (
-//     <Box pb={8}>
-//       <Stack
-//         pos="relative"
-//         bgGradient={`linear(to-l, blue.500, blue.400 , cyan.400)`}
-//         height="250px"
-//         w="100%"
-//       ></Stack>
-//       <Box maxW="3xl" p={4} isolation="isolate" zIndex={3} mt="-10rem" marginInline="auto">
-//         <Box
-//           boxShadow={useColorModeValue(
-//             '0 4px 6px rgba(160, 174, 192, 0.6)',
-//             '0 4px 6px rgba(9, 17, 28, 0.9)'
-//           )}
-//           bg={useColorModeValue('white', 'gray.800')}
-//           p={{ base: 4, sm: 8 }}
-//           overflow="hidden"
-//           rounded="2xl"
-//         >
-//           <Stack pos="relative" zIndex={1} direction="column" spacing={5} textAlign="left">
-//             <chakra.h1 fontSize="4xl" lineHeight={1.2} fontWeight="bold">
-//               Welcome back {displayName}!
-//             </chakra.h1>
-//             <chakra.h1 color="gray.400" fontSize="xl" maxW="600px" lineHeight={1.2}>
-//               Kudos to you! You are one of the 10% saving energy. Your meter information can be viewed below.
-//             </chakra.h1>
-
-            
-//           </Stack>
-//         </Box>
-//       </Box>
-//     </Box>
-//   );
-// };
-
-// export default Hero;
-
-
 
 import { chakra, Link, Stack, Box, Button, useColorModeValue, Text, Flex } from '@chakra-ui/react';
 import { FaGithub } from 'react-icons/fa';
@@ -131,7 +87,7 @@ const Hero = ({displayName}) => {
                 bgGradient="linear(to-l, blue.500, cyan.400)"
                 bgClip="text"
               >
-                IoT-Enabled Monitoring System for Photovoltaic Systems.
+                Energy monitoring system
               </chakra.h1>
             </Flex>
 
@@ -143,9 +99,9 @@ const Hero = ({displayName}) => {
               fontFamily="Nunito, sans-serif"
               fontWeight="500"
             >
-              Designed and Implemented by Mechatronics Engineering Students of FUTO, Nigeria; December, 2024.
+              Designed and Implemented by -------------, Nigeria; December, 2024.
             </chakra.h2>
-            <chakra.h2 
+            {/* <chakra.h2 
               color="gray.500" 
               fontSize={["lg", "xl"]} 
               maxW="600px" 
@@ -154,7 +110,7 @@ const Hero = ({displayName}) => {
               fontWeight="500"
             >
               Supervisor: Engr. Dr. E.S. Mbonu.
-            </chakra.h2>
+            </chakra.h2> */}
           </Stack>
         </Box>
       </Box>
